056b4ef39f57ad5de39a23b2c386ce1662a7b6f0
Eduardo <70926975+librotero@users.noreply.github.com>
2022-06-15 17:22:06 -0300

    actualizacion y sincornizacion de node y postgres

---
+++ client/src/components/inicio/Inicio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Inicio = () => {
+    return ( <
+        div className = "" >
+        <
+        div >
+        <
+        Link to = "/inicio" > < /Link>{" "} < /
+        div > { " " } <
+        /div>
+    );
+};
+
+export default Inicio;
